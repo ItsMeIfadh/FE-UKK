7da2e6b8defc9aef20ade6e01f46e6ab23c37b18
ItsMeIfadh <itsmeifadh@gmail.com>
2025-04-14 09:58:19 +0700

    fix: missing import file in routes

--- src/pages/components/mui/alert.jsx
+++ src/pages/components/mui/alert.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 // sections
-import AlertView from 'src/sections/_examples/mui/alert-view';
+// import AlertView from 'src/sections/_examples/mui/alert-view';
 
 // ----------------------------------------------------------------------
 
@@ -11,7 +11,7 @@ export default function AlertPage() {
         <title> MUI: Alert</title>
       </Helmet>
 
-      <AlertView />
+      {/* <AlertView /> */}
     </>
   );
 }
