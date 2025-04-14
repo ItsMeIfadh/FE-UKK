5d0696386f694a5a979f6ca243d6193b5bc81561
ItsMeIfadh <itsmeifadh@gmail.com>
2025-04-14 10:33:11 +0700

    fix: missing import file in routes

--- src/routes/sections/auth.jsx
+++ src/routes/sections/auth.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 // auth
 import { GuestGuard } from 'src/auth/guard';
 // layouts
-import CompactLayout from 'src/layouts/compact';
+// import CompactLayout from 'src/layouts/compact';
 import AuthClassicLayout from 'src/layouts/auth/classic';
 // components
 import { SplashScreen } from 'src/components/loading-screen';
@@ -11,25 +11,25 @@ import { SplashScreen } from 'src/components/loading-screen';
 // ----------------------------------------------------------------------
 
 // AMPLIFY
-const AmplifyLoginPage = lazy(() => import('src/pages/auth/amplify/login'));
-const AmplifyRegisterPage = lazy(() => import('src/pages/auth/amplify/register'));
-const AmplifyVerifyPage = lazy(() => import('src/pages/auth/amplify/verify'));
-const AmplifyNewPasswordPage = lazy(() => import('src/pages/auth/amplify/new-password'));
-const AmplifyForgotPasswordPage = lazy(() => import('src/pages/auth/amplify/forgot-password'));
+// const AmplifyLoginPage = lazy(() => import('src/pages/auth/amplify/login'));
+// const AmplifyRegisterPage = lazy(() => import('src/pages/auth/amplify/register'));
+// const AmplifyVerifyPage = lazy(() => import('src/pages/auth/amplify/verify'));
+// const AmplifyNewPasswordPage = lazy(() => import('src/pages/auth/amplify/new-password'));
+// const AmplifyForgotPasswordPage = lazy(() => import('src/pages/auth/amplify/forgot-password'));
 
 // JWT
 const JwtLoginPage = lazy(() => import('src/pages/auth/jwt/login'));
 const JwtRegisterPage = lazy(() => import('src/pages/auth/jwt/register'));
 
 // FIREBASE
-const FirebaseLoginPage = lazy(() => import('src/pages/auth/firebase/login'));
-const FirebaseRegisterPage = lazy(() => import('src/pages/auth/firebase/register'));
-const FirebaseVerifyPage = lazy(() => import('src/pages/auth/firebase/verify'));
-const FirebaseForgotPasswordPage = lazy(() => import('src/pages/auth/firebase/forgot-password'));
+// const FirebaseLoginPage = lazy(() => import('src/pages/auth/firebase/login'));
+// const FirebaseRegisterPage = lazy(() => import('src/pages/auth/firebase/register'));
+// const FirebaseVerifyPage = lazy(() => import('src/pages/auth/firebase/verify'));
+// const FirebaseForgotPasswordPage = lazy(() => import('src/pages/auth/firebase/forgot-password'));
 
-// AUTH0
-const Auth0LoginPage = lazy(() => import('src/pages/auth/auth0/login'));
-const Auth0Callback = lazy(() => import('src/pages/auth/auth0/callback'));
+// // AUTH0
+// const Auth0LoginPage = lazy(() => import('src/pages/auth/auth0/login'));
+// const Auth0Callback = lazy(() => import('src/pages/auth/auth0/callback'));
 
 // ----------------------------------------------------------------------
 
@@ -42,36 +42,6 @@ const authAmplify = {
       </Suspense>
     </GuestGuard>
   ),
-  children: [
-    {
-      path: 'login',
-      element: (
-        <AuthClassicLayout>
-          <AmplifyLoginPage />
-        </AuthClassicLayout>
-      ),
-    },
-    {
-      path: 'register',
-      element: (
-        <AuthClassicLayout title="Manage the job more effectively with Minimal">
-          <AmplifyRegisterPage />
-        </AuthClassicLayout>
-      ),
-    },
-    {
-      element: (
-        <CompactLayout>
-          <Outlet />
-        </CompactLayout>
-      ),
-      children: [
-        { path: 'verify', element: <AmplifyVerifyPage /> },
-        { path: 'new-password', element: <AmplifyNewPasswordPage /> },
-        { path: 'forgot-password', element: <AmplifyForgotPasswordPage /> },
-      ],
-    },
-  ],
 };
 
 const authJwt = {
@@ -112,35 +82,6 @@ const authFirebase = {
       </Suspense>
     </GuestGuard>
   ),
-  children: [
-    {
-      path: 'login',
-      element: (
-        <AuthClassicLayout>
-          <FirebaseLoginPage />
-        </AuthClassicLayout>
-      ),
-    },
-    {
-      path: 'register',
-      element: (
-        <AuthClassicLayout title="Manage the job more effectively with Minimal">
-          <FirebaseRegisterPage />
-        </AuthClassicLayout>
-      ),
-    },
-    {
-      element: (
-        <CompactLayout>
-          <Outlet />
-        </CompactLayout>
-      ),
-      children: [
-        { path: 'verify', element: <FirebaseVerifyPage /> },
-        { path: 'forgot-password', element: <FirebaseForgotPasswordPage /> },
-      ],
-    },
-  ],
 };
 
 const authAuth0 = {
@@ -152,20 +93,6 @@ const authAuth0 = {
       </Suspense>
     </GuestGuard>
   ),
-  children: [
-    {
-      path: 'login',
-      element: (
-        <AuthClassicLayout>
-          <Auth0LoginPage />
-        </AuthClassicLayout>
-      ),
-    },
-    {
-      path: 'callback',
-      element: <Auth0Callback />,
-    },
-  ],
 };
 
 export const authRoutes = [
