8c9876bea9bd3688341c56de9e4cccc36cb34d11
ItsMeIfadh <itsmeifadh@gmail.com>
2025-04-14 15:19:41 +0700

    perbaikan page contact us

--- src/sections/faqs/faqs-hero.jsx
+++ src/sections/faqs/faqs-hero.jsx
@@ -49,26 +49,26 @@ export default function FaqsHero() {
             <br />
 
             <Stack spacing={1} sx={{ color: 'common.white' }}>
-  <Stack
-    direction="row"
-    spacing={2}
-    display="inline-flex"
-    sx={{
-      flexWrap: 'wrap',
-    }}
-  >
-    <TextAnimate text="Yuk," />
-    <TextAnimate text="kita" />
-    <TextAnimate text="kupas" />
-  </Stack>
+              <Stack
+                direction="row"
+                spacing={2}
+                display="inline-flex"
+                sx={{
+                  flexWrap: 'wrap',
+                }}
+              >
+                <TextAnimate text="Yuk," />
+                <TextAnimate text="kita" />
+                <TextAnimate text="kupas" />
+              </Stack>
 
-  <TextAnimate
-    text="tuntas!"
-    sx={{
-      fontWeight: 'bold',
-    }}
-  />
-</Stack>
+              <TextAnimate
+                text="tuntas!"
+                sx={{
+                  fontWeight: 'bold',
+                }}
+              />
+            </Stack>
 
           </div>
 
