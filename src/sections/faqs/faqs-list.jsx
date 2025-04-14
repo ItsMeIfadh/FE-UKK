5130af9efb6415e3375ce331ca7db94dd3732dcb
ItsMeIfadh <itsmeifadh@gmail.com>
2025-04-14 15:24:01 +0700

    perbaikan page contact us

--- src/sections/faqs/faqs-list.jsx
+++ src/sections/faqs/faqs-list.jsx
@@ -1,4 +1,3 @@
-// @mui
 import Accordion from '@mui/material/Accordion';
 import Typography from '@mui/material/Typography';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -8,19 +7,51 @@ import { _faqs } from 'src/_mock';
 // components
 import Iconify from 'src/components/iconify';
 
-// ----------------------------------------------------------------------
-
 export default function FaqsList() {
   return (
     <div>
       {_faqs.map((accordion) => (
-        <Accordion key={accordion.id}>
+        <Accordion 
+          key={accordion.id}
+          sx={{
+            mb: 2,
+            '& .MuiAccordionSummary-root': {
+              px: { xs: 2, sm: 3, md: 4 },
+              py: { xs: 1, sm: 2 },
+            },
+            '& .MuiAccordionDetails-root': {
+              px: { xs: 2, sm: 3, md: 4 },
+              py: { xs: 1, sm: 2 },
+            },
+          }}
+        >
           <AccordionSummary expandIcon={<Iconify icon="eva:arrow-ios-downward-fill" />}>
-            <Typography variant="subtitle1">{accordion.heading}</Typography>
+            <Typography
+              variant="subtitle1"
+              sx={{
+                fontSize: {
+                  xs: '1rem',
+                  sm: '1.1rem',
+                  md: '1.25rem',
+                },
+                fontWeight: 'bold',
+              }}
+            >
+              {accordion.heading}
+            </Typography>
           </AccordionSummary>
 
           <AccordionDetails>
-            <Typography>{accordion.detail}</Typography>
+            <Typography
+              sx={{
+                fontSize: {
+                  xs: '0.875rem',
+                  sm: '1rem',
+                },
+              }}
+            >
+              {accordion.detail}
+            </Typography>
           </AccordionDetails>
         </Accordion>
       ))}
